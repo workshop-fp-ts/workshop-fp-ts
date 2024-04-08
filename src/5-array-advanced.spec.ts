f47e74d0d2baf5b55070cd8d2892d3bbeab59d9f
Johan Rouve <johan.rouve@comet.team>
2024-04-08 17:06:31 +0200

    Add array advanced

--- src/5-array-advanced.spec.ts
+++ src/5-array-advanced.spec.ts
@@ -1,4 +1,7 @@
+import * as A from "fp-ts/Array";
+import * as E from "fp-ts/Either";
 import { pipe } from "fp-ts/function";
+import * as N from "fp-ts/number";
 import { describe, expect, it } from "vitest";
 import { TO_REPLACE } from "./utils";
 
@@ -40,6 +43,156 @@ describe("Array Advanced", () => {
 
     // ⬇⬇⬇⬇ Code here ⬇⬇⬇⬇
 
+    const resultWithPipe = pipe(input, TO_REPLACE);
+
+    // ⬆⬆⬆⬆ Code here ⬆⬆⬆⬆
+
+    expect(resultWithPipe).toEqual(result);
+  });
+
+  it.skip("should separate items in 2 groups", () => {
+    type InvalidResult = { success: false; message: string };
+    type ValidResult = { success: true; data: number };
+    type Result = ValidResult | InvalidResult;
+    type All = Result[];
+    const input: All = [
+      { success: false, message: "foo is not a number" },
+      { success: true, data: 11 },
+      { success: false, message: "another error message" },
+      { success: true, data: 31 },
+    ];
+    const isValidResult = (d: Result): d is ValidResult => d.success;
+
+    const result = input.reduce(
+      (acc, item) => {
+        if (isValidResult(item)) {
+          return { items: [...acc.items, item], errors: acc.errors };
+        }
+        return { items: acc.items, errors: [...acc.errors, item] };
+      },
+      { items: [] as ValidResult[], errors: [] as InvalidResult[] }
+    );
+
+    // ⬇⬇⬇⬇ Code here ⬇⬇⬇⬇
+
+    const resultWithPipe = pipe(input, TO_REPLACE);
+
+    // ⬆⬆⬆⬆ Code here ⬆⬆⬆⬆
+
+    expect(resultWithPipe).toEqual(result);
+  });
+
+  it.skip("should separate from an Either's list", () => {
+    type InvalidResult = { message: string };
+    type ValidResult = { data: number };
+    type Result = E.Either<InvalidResult, ValidResult>;
+    type All = Result[];
+    const input: All = [
+      E.left({ message: "foo is not a number" }),
+      E.right({ data: 11 }),
+      E.left({ message: "another error message" }),
+      E.right({ data: 31 }),
+    ];
+
+    // ⬇⬇⬇⬇ Code here ⬇⬇⬇⬇
+
+    const resultWithPipe = pipe(input, TO_REPLACE);
+
+    // ⬆⬆⬆⬆ Code here ⬆⬆⬆⬆
+
+    expect(resultWithPipe).toEqual({
+      left: [
+        { message: "foo is not a number" },
+        { message: "another error message" },
+      ],
+      right: [{ data: 11 }, { data: 31 }],
+    });
+  });
+
+  it.skip("should find item in a list", () => {
+    const input = [
+      { position: 3, message: "foo" },
+      { position: 11, message: "bar" },
+      { position: 31, message: "gg" },
+      { position: 23, message: "another" },
+    ];
+    const isFooItem = (item: { message: string }) => item.message === "foo";
+
+    const foo = input.find(isFooItem);
+    let result = 0;
+    if (foo) {
+      const position = foo.position;
+      result = position + 1;
+    }
+
+    // ⬇⬇⬇⬇ Code here ⬇⬇⬇⬇
+
+    const resultWithPipe = pipe(input, TO_REPLACE);
+
+    // ⬆⬆⬆⬆ Code here ⬆⬆⬆⬆
+
+    expect(resultWithPipe).toEqual(result);
+  });
+
+  it.skip("should dedupe simple items in a list", () => {
+    const input = [1, 3, 6, 8, 3];
+
+    const result = Array.from(new Set(input));
+
+    // ⬇⬇⬇⬇ Code here ⬇⬇⬇⬇
+
+    const resultWithPipe = pipe(input, A.uniq(N.Eq));
+
+    // ⬆⬆⬆⬆ Code here ⬆⬆⬆⬆
+
+    expect(resultWithPipe).toEqual(result);
+  });
+
+  it.skip("should dedupe objects in a list", () => {
+    const input = [
+      { position: 4 },
+      { position: 1 },
+      { position: 12 },
+      { position: 8 },
+    ];
+
+    const result = input.filter(
+      (value, index) =>
+        index === input.findIndex((item) => item.position === value.position)
+    );
+
+    // ⬇⬇⬇⬇ Code here ⬇⬇⬇⬇
+    const resultWithPipe = pipe(input, TO_REPLACE);
+
+    // ⬆⬆⬆⬆ Code here ⬆⬆⬆⬆
+
+    expect(resultWithPipe).toEqual(result);
+  });
+
+  it.skip("should sort simple items in a list", () => {
+    const input = [1, 3, 6, 8, 3];
+
+    const result = input.sort();
+
+    // ⬇⬇⬇⬇ Code here ⬇⬇⬇⬇
+    const resultWithPipe = pipe(input, TO_REPLACE);
+
+    // ⬆⬆⬆⬆ Code here ⬆⬆⬆⬆
+
+    expect(resultWithPipe).toEqual(result);
+  });
+
+  it.skip("should sort objects in a list", () => {
+    const input = [
+      { position: 4 },
+      { position: 1 },
+      { position: 12 },
+      { position: 8 },
+    ];
+
+    const result = input.sort((a, b) => a.position - b.position);
+
+    // ⬇⬇⬇⬇ Code here ⬇⬇⬇⬇
     const resultWithPipe = pipe(input, TO_REPLACE);
 
     // ⬆⬆⬆⬆ Code here ⬆⬆⬆⬆
