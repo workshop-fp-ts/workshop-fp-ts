cc37953db0714fbc8dcc47655841c6cde4b59032
Johan Rouve <johan.rouve@comet.team>
2024-04-09 10:28:31 +0200

    Add do notation exercice

---
+++ src/9-do-notation.spec.ts
@@ -0,0 +1,98 @@
+import * as A from "fp-ts/Array";
+import * as T from "fp-ts/Task";
+import { pipe } from "fp-ts/function";
+import { describe, expect, it } from "vitest";
+import { TO_REPLACE } from "./utils";
+
+/**
+ * https://gcanti.github.io/fp-ts/modules/Task.ts.html
+ *
+ * Do notation can be used to simplify your code
+ */
+describe("Do notation", () => {
+  it.skip("The do notation help you create a context", async () => {
+    const authorId = 42;
+
+    const taskToSimplify = pipe(
+      getAuthor(authorId),
+      T.flatMap((author) =>
+        pipe(
+          getBooksByAuthor(author.id),
+          T.map((books) => ({ author, books }))
+        )
+      ),
+      T.flatMap(({ author, books }) =>
+        pipe(
+          getSimilarAuhtors(author.id),
+          T.map((similarAuhtors) => ({ author, books, similarAuhtors }))
+        )
+      ),
+      T.map(toAuthorDto)
+    );
+
+    // ⬇⬇⬇⬇ Code here ⬇⬇⬇⬇
+    const taskWithDoNotation = pipe(
+      getAuthor(authorId),
+      TO_REPLACE,
+      T.map(toAuthorDto)
+    );
+    // ⬆⬆⬆⬆ Code here ⬆⬆⬆⬆
+
+    const result = await taskToSimplify();
+    const resultWithDoNotation = await taskWithDoNotation();
+    expect(result).toEqual({
+      id: 42,
+      name: "John Doe",
+      booksCount: 3,
+      recommendedAuthors: ["Jane Doe"],
+    });
+    expect(result).toEqual(resultWithDoNotation);
+  });
+});
+
+type Author = { id: number; name: string };
+type SimilarAuthor = Author & { similarTo: number };
+type Book = { id: number; title: string; authorId: number };
+type AuthorDto = {
+  id: number;
+  name: string;
+  booksCount: number;
+  recommendedAuthors: string[];
+};
+
+const getBooksByAuthor =
+  (id: number): T.Task<Book[]> =>
+  () =>
+    Promise.resolve([
+      { id: 1, title: "My first book", authorId: id },
+      { id: 2, title: "This is the end", authorId: id },
+      { id: 3, title: "I need more money", authorId: id },
+    ]);
+
+const getAuthor =
+  (id: number): T.Task<Author> =>
+  () =>
+    Promise.resolve({ id, name: `John Doe` });
+
+const getSimilarAuhtors =
+  (id: number): T.Task<SimilarAuthor[]> =>
+  () =>
+    Promise.resolve([{ id: 37, name: "Jane Doe", similarTo: id }]);
+
+const toAuthorDto = ({
+  author,
+  books,
+  similarAuhtors,
+}: {
+  author: Author;
+  books: Book[];
+  similarAuhtors: SimilarAuthor[];
+}): AuthorDto => ({
+  id: author.id,
+  name: author.name,
+  booksCount: books.length,
+  recommendedAuthors: pipe(
+    similarAuhtors,
+    A.map((author) => author.name)
+  ),
+});
